8523cec6e39fec23a88cf499fbc96d6601e2eb56
izhang1 <ivanzhangemail@gmail.com>
2015-08-09 18:09:14 -0400

    Added ability in loading page to go to home page

--- app/src/main/java/com/example/izhang/roomator/loading.java
+++ app/src/main/java/com/example/izhang/roomator/loading.java
@@ -35,7 +35,45 @@ public class loading extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         Firebase.setAndroidContext(getApplicationContext());
-        isNetworkAvailable();
+
+        TextView loadingText = (TextView) findViewById(R.id.textViewLoading);
+
+        // Checks to see if the network is available, shows toast if it is not
+        if(isNetworkAvailable() == false) {
+            loadingText.setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(), "Please enable internet", Toast.LENGTH_LONG).show();
+            Log.d("NetworkAvailable", "Hello");
+        }else {
+
+            // Setup Firebase
+            final Firebase myFirebaseRef = new Firebase("https://roomator.firebaseio.com/");
+
+            // Get android_id
+            final String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+
+            myFirebaseRef.child("didlogin").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.child(android_id).exists()) {
+                        Toast.makeText(getApplicationContext(), "You are all set to go.", Toast.LENGTH_LONG).show();
+                        Intent mainIntent = new Intent(getApplicationContext(), main.class);
+                        startActivity(mainIntent);
+                        finish();
+                    } else {
+                        Intent loginIntent = new Intent(getApplicationContext(), login.class);
+                        startActivity(loginIntent);
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+
+        }
     }
 
     // Network method to check if there is connetion to internet or not
