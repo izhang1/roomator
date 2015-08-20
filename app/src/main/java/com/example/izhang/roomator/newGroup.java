647045d8aa4598b48482d9473254e68f3ef88b3d
izhang1 <ivanzhangemail@gmail.com>
2015-08-20 19:30:08 -0400

    New initial code in newGroup to initialize buttons and edittext fields. Added new todos

--- app/src/main/java/com/example/izhang/roomator/newGroup.java
+++ app/src/main/java/com/example/izhang/roomator/newGroup.java
@@ -1,10 +1,16 @@
 package com.example.izhang.roomator;
 
 import android.app.Activity;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 
 public class newGroup extends Activity {
 
@@ -12,6 +18,37 @@ public class newGroup extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_group);
+        Firebase.setAndroidContext(getApplicationContext());
+
+        // Setup Firebase
+        final Firebase myFirebaseRef = new Firebase("https://roomator.firebaseio.com/");
+
+        // Get android_id
+        final String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        Button joinGroupButton = (Button) this.findViewById(R.id.joinGroupButton);
+        Button newGroupButton = (Button) this.findViewById(R.id.addGroupButton);
+        EditText newGroupName = (EditText) this.findViewById(R.id.groupName);
+        EditText joinGroupId = (EditText) this.findViewById(R.id.groupID);
+
+        //todo: When this button is clicked, verify group with joinGroupId. If there, add that group to the user's account. New activity to navi.
+        joinGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //todo: When this button is clicked, verify that the name is new. Create a new group and add the user to that group. New activity to navi.
+        newGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 
 
