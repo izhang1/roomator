5441d320447e6d00b8d1ad23710c79dab4769304
izhang1 <ivanzhangemail@gmail.com>
2015-09-28 09:29:30 -0400

    Propogate change to the db

--- app/src/main/java/com/example/izhang/roomator/chores.java
+++ app/src/main/java/com/example/izhang/roomator/chores.java
@@ -43,6 +43,7 @@ public class chores extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static String account_id = "";
+    private int choresCount;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -112,13 +113,12 @@ public class chores extends Fragment {
                 android.R.layout.simple_list_item_1, myStringArray);
         choresList.setAdapter(adapter);
 
-        //TODO: Use account id to get groupID, use id of group to get chorelist and add them to adapter
         myFirebaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(final DataSnapshot dataSnapshot) {
-                String groupID = dataSnapshot.child("account").child(account_id).child("group").getValue().toString();
-                int choresCount = 1;
-                Iterable<DataSnapshot> choreIter = dataSnapshot.child("group").child(groupID).child("chores").child("todo").getChildren();
+                final String groupID = dataSnapshot.child("account").child(account_id).child("group").getValue().toString();
+                choresCount = 1;
+                final Iterable<DataSnapshot> choreIter = dataSnapshot.child("group").child(groupID).child("chores").child("todo").getChildren();
                 Log.d("Chores", "groupID " + groupID + " Account id:" + account_id + "  :Before forLoop");
                 for (DataSnapshot d : choreIter) {
                     myStringArray.add(d.child("title").getValue().toString());
@@ -146,7 +146,8 @@ public class chores extends Fragment {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 m_Text = input.getText().toString();
-                                //TODO: Add this to the chores.todo with the person who created it "created by", "dateCreated" and "title"
+                                myFirebaseRef.child("group").child(groupID).child("chores").child("todo").child(Integer.toString(choresCount)).child("title").setValue(m_Text);
+                                myFirebaseRef.child("group").child(groupID).child("chores").child("todo").child(Integer.toString(choresCount)).child("createdBy").setValue(account_id);
                             }
                         });
                         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -159,7 +160,6 @@ public class chores extends Fragment {
                         builder.show();
                     }
                 });
-
 
             }
 
@@ -209,7 +209,7 @@ public class chores extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+        // TODO: Update argument type and names
         public void onFragmentInteraction(Uri uri);
     }
 
