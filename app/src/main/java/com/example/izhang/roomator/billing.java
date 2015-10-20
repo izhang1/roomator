9a662af0e78c19432fc4414bfe567b8bf9a3c965
izhang1 <ivanzhangemail@gmail.com>
2015-10-19 21:07:15 -0400

    Added venmo libraries to Roomator

--- app/src/main/java/com/example/izhang/roomator/billing.java
+++ app/src/main/java/com/example/izhang/roomator/billing.java
@@ -182,10 +182,7 @@ public class billing extends Fragment {
                     public void onClick(DialogInterface dialog, int which) {
                         description = descriptionInput.getText().toString();
                         cost = costInput.getText().toString();
-
                         Toast.makeText(getActivity(), "Description: " + description + "  CostPerPerson: " + cost, Toast.LENGTH_LONG).show();
-
-
                         //adapter.setNotifyOnChange(true);
                     }
                 });
