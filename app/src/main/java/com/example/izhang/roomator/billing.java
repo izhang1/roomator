9b436193f7d72a069bc21375a333661e336eeb32
izhang1 <ivanzhangemail@gmail.com>
2015-10-25 22:19:03 -0400

    * Removed multiple comments and todos automatically generated by Android Studio

--- app/src/main/java/com/example/izhang/roomator/billing.java
+++ app/src/main/java/com/example/izhang/roomator/billing.java
@@ -39,7 +39,6 @@ public class billing extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    // Global variables for fragment view
     ListView billList;
     View view;
     private String venmo_appID = "";
@@ -256,18 +255,7 @@ public class billing extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
     }
 
